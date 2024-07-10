ca710cc5b8cf2a06e42e1d60c41e7987e6ea69d7
Charles Woods <159830290+CwoodsTW@users.noreply.github.com>
2024-07-10 16:25:55 -0500

    added pagination and fixed search feature

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
-import { useDebounce, useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -10,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
-    
+    params.set('page', '1');
     if(term) params.set('query', term);
     else params.delete('query');
 
@@ -33,4 +33,3 @@ export default function Search({ placeholder }: { placeholder: string }) {
   );
 }
 
-//chapter 11, adding pagination
